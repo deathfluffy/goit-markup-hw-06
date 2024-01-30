ac9fab5a937729a7d627f66f8be922b72588a181
deathfluffy <mrandreu454@gmail.com>
2024-01-30 22:46:02 +0200

    FIX

--- js/modal.js
+++ js/modal.js
@@ -4,6 +4,7 @@
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
+ 
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
